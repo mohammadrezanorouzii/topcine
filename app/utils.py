652e73b6e5e8946468b2dbc1e4a49967147ac3af
Ali <AliAsadiWasTaken@gmail.com>
2022-08-26 12:47:29 +0430

    fixed and adjusted the back-end side of the project

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_contex = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash(password : str):
-    return pwd_contex.hash(password)
-
-def verify(plain_password , hashed_password):
-    return pwd_contex.verify(plain_password, hashed_password)
